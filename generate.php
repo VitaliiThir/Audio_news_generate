41b5807ecc99c4e7cce0831b348191571a42a73b
VitaliiThir <Vitslikus865@gmail.com>
2022-08-28 21:17:51 +0400

    Last

--- generate.php
+++ generate.php
@@ -2,8 +2,6 @@
 
 use Bitrix\Main\Application;
 
-if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die();
-
 require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/main/include/prolog_before.php");
 header('Content-type: application/json');
 ?>
--- index.php
+++ index.php
@@ -1,6 +1,4 @@
 <?
-if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die();
-
 require_once($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/main/include/prolog_before.php");
 
 global $USER;
